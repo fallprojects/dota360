d5a02a0b54c4077f8be1b558cc319fabcfbea232
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-17 13:01:04 +0600

    created heroes list function with filter

--- dota360/appdota/views.py
+++ dota360/appdota/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .forms import *
+from .models import *
+from django_filters import OrderingFilter
+from .filters import heroFilter
 # Create your views here.
+
+def heroList(request):
+    heroes = Hero.objects.all()
+    filter = heroFilter(request.GET, queryset=heroes)
+    heroes = filter.qs
+    context = {'heroes': heroes, 'filter': filter}
+    return render(request, 'heroes.html', context)
