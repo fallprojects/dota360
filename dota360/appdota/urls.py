35be21e49eaaa41294f9fa88b48902adffeb74ff
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-18 12:49:09 +0600

    created url path for her info

--- dota360/appdota/urls.py
+++ dota360/appdota/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from .models import *
 
 urlpatterns = [
     path('heroes/',heroList,name='heroes'),
+    path('heroinfo/<str:hero_name>/',heroInfo,name='heroinfo'),
+    # path('herobuild/',name='herobuild'),
 ]
