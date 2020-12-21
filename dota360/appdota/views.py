c1e880fc5217c30655d9876da5999a8e4975f03b
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-21 13:04:45 +0600

    created guidecreate,guidelist, and guide view functions

--- dota360/appdota/views.py
+++ dota360/appdota/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import *
 from .models import *
@@ -23,5 +23,30 @@ def heroInfo(request,hero_name):
             return HttpResponse('Theres no such hero.404')
         context = {'hero': hero}
         return render(request, 'hero-info.html', context)
+
+
+
+def guideCreate(request):
+    form = GuideForm()
+    if request.method == 'POST':
+        form = GuideForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'guide_form': form}
+    return render(request, 'guide-create.html', context)
+
+
+def guideList(request):
+    guides = Guide.objects.all()
+    context = {'guides': guides}
+    return render(request,'guide-list.html',context)
+
+def guideView(request,hero_name):
+    try:
+        guide = Guide.objects.get(hero__name=hero_name)
+    except Hero.DoesNotExist:
+        return HttpResponse('Theres no such hero.404')
+    context = {'guide': guide}
+    return render(request, 'guide-view.html', context)
 
 
